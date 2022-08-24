fdb82570bc35d64f6c56bf60a484dc3ab500181f
Juan Salazar <juan@sbl.dev>
2022-08-24 14:03:57 -0500

    Add rive animation to project

--- flutter_rive/lib/main.dart
+++ flutter_rive/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rive/modules/plant/plant.dart';
 
-import 'modules/home/home.dart';
+import 'core/utils/color_util.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,39 +10,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: "Mi App",
-      home: Inicio(
-        tittle: 'My App',
+      debugShowCheckedModeBanner: false,
+      home: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [
+                  HexColor("7f00b2").withOpacity(0.7),
+                  HexColor("8e22bb").withOpacity(0.5),
+                  HexColor("ab49cc").withOpacity(0.4)
+                ],
+                begin: const FractionalOffset(0.0, 0.0),
+                end: const FractionalOffset(0.0, 1.0),
+                stops: const [0.0, 0.5, 1.0],
+                tileMode: TileMode.clamp)),
+        child: const PlantScreen(),
       ),
     );
-  }
-}
-
-class Inicio extends StatefulWidget {
-  const Inicio({super.key, required this.tittle});
-  final String tittle;
-  @override
-  State<Inicio> createState() => _InicioState();
-}
-
-class _InicioState extends State<Inicio> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text(widget.tittle)),
-        body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text("Bienvenido a la app"),
-            ElevatedButton(
-                child: const Text("Ir a la otra pagina"),
-                onPressed: () => {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => const Home()))
-                    })
-          ],
-        )));
   }
 }
